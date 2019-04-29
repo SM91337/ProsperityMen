0f64540ca56d010dcc12322fd62a3e7ac1d359fa
SM9 <sm9fragdeluxe@gmail.com>
2019-04-29 05:05:13 +0100

    Fix config.

--- src/main/java/com/sm9/prosperitymen/common/Config/MainConfig.java
+++ src/main/java/com/sm9/prosperitymen/common/Config/MainConfig.java
@@ -10,7 +10,7 @@ public class MainConfig {
     public static boolean debugMode = false;
     public static Configuration mainConfig;
     public static boolean meleeOnly = true;
-    public static boolean netherOnly = true;
+    public static boolean netherOnly = false;
     public static boolean worldRandom = true;
     public static int dropChance = 50;
     public static int agroRange = 48;
@@ -18,11 +18,11 @@ public class MainConfig {
     public static void loadConfig() {
         mainConfig.load();
         mainConfig.setCategoryPropertyOrder("General", configOrder);
-        debugMode = mainConfig.getBoolean("DebugMode", Configuration.CATEGORY_GENERAL, false, "Prints debug information on hostile entity death");
-        dropChance = mainConfig.getInt("DropChance", Configuration.CATEGORY_GENERAL, 50, 0, 100, "Drop chance for hostiles");
-        meleeOnly = mainConfig.getBoolean("MeleeOnly", Configuration.CATEGORY_GENERAL, meleeOnly, "Only allow essence drops when killed with a melee weapon");
-        netherOnly = mainConfig.getBoolean("NetherOnly", Configuration.CATEGORY_GENERAL, false, "Pigmen will only agro / drop prosperity in the Nether");
-        agroRange = mainConfig.getInt("HotbarAgroRange", Configuration.CATEGORY_GENERAL, 48, 0, 128, "Range at which pigmen sense you are holding their precious shards, 0 turns this feature off");
+        debugMode = mainConfig.getBoolean("DebugMode", Configuration.CATEGORY_GENERAL, debugMode, "Prints debug information on zombie pigmen death");
+        dropChance = mainConfig.getInt("DropChance", Configuration.CATEGORY_GENERAL, dropChance, 0, 100, "Drop chance for zombie pigmen");
+        meleeOnly = mainConfig.getBoolean("MeleeOnly", Configuration.CATEGORY_GENERAL, meleeOnly, "Only allow prosperity drops when killed with a melee weapon");
+        netherOnly = mainConfig.getBoolean("NetherOnly", Configuration.CATEGORY_GENERAL, netherOnly, "Pigmen will only agro / drop prosperity in the Nether");
+        agroRange = mainConfig.getInt("HotbarAgroRange", Configuration.CATEGORY_GENERAL, agroRange, 0, 128, "Range at which pigmen sense you are holding their precious shards, 0 turns this feature off");
         worldRandom = mainConfig.getBoolean("WorldRandom", Configuration.CATEGORY_GENERAL, worldRandom, "Use world random generator instead of built in java one");
         mainConfig.save();
     }
