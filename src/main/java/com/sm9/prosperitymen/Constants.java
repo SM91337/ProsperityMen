26827721ddd2c3fca7813957ceef0089b0511eff
SM9CC <me@sm9.dev>
2019-06-04 20:28:30 +0100

    Workspace stuff, Gradle upgrade, Ask to load after all mods nicely.

--- src/main/java/com/sm9/prosperitymen/Constants.java
+++ src/main/java/com/sm9/prosperitymen/Constants.java
@@ -5,7 +5,5 @@ class Constants {
     public static final String MOD_NAME = "Prosperity Men";
     public static final String MOD_VERSION = "0.2";
     public static final String MINECRAFT_VERSION = "1.12.2";
-    private static final String MIN_FORGE_VER = "14.23.5.2836";
-    public static final String DEPENDENCIES = "after:forge@[" + MIN_FORGE_VER + ",)" +
-            ";required-after:mysticalagriculture@[1.7.0,)";
+    static final String DEPENDENCIES = "after:forge@[" + "14.23.5.2838" + ",)" + ";after:*";
 }
